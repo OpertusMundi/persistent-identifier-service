b91fb2d0775a35715cc63634799f9b5dd1f330fa
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-06-15 11:52:18 +0200

    Added DB setup code

---
+++ ompid/db/__init__.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
+import ompid
+
+
+def build_postgresql_url(settings):
+    pg_settings = settings['postgresql']
+    return f'postgresql://' \
+        f'{pg_settings["user"]}:{pg_settings["password"]}' \
+        f'@{pg_settings["host"]}:{pg_settings["port"]}' \
+        f'/{pg_settings["db"]}'
+
+
+engine = create_engine(
+    build_postgresql_url(ompid.load_default_configuration()),
+    echo=True)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
