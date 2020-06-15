cbe064fa84320fa67b5cc88e5f18739449fae1a0
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-06-15 11:50:09 +0200

    Added setup.py and settings template

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='persistent-identifier-service',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='',
+    description='',
+    install_requires=[
+        'fastapi==0.55.1',
+        'uvicorn==0.11.5',
+        'SQLAlchemy==1.3.17',
+        'PyYAML==5.3.1',
+        'pydantic==1.5.1',
+        'psycopg2==2.8.5',
+    ]
+)
