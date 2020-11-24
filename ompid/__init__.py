6aaa54315d5a20a9628fcb8347f7d1e1d33783ae
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-11-24 17:02:04 +0100

    Multiple updates to introduce namespace idntifiers
    Identifiers will now be built using a user namespace, an asset type and
    the actual asset ID, e.g. user23.dataset.567

--- ompid/__init__.py
+++ ompid/__init__.py
@@ -1,11 +1,14 @@
 import os
+from typing import List
 
 import yaml
 from fastapi import FastAPI
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
-from ompid.models import OMService, OMServiceCreate, OMServiceORM, Base
+from ompid.models import Base, TopioUser, TopioUserCreate, TopioUserORM, \
+    TopioAssetType, TopioAssetTypeORM, TopioAsset, TopioAssetORM, \
+    TopioAssetCreate, TopioUserQuery
 
 
 def load_default_configuration():
@@ -33,22 +36,90 @@ app = FastAPI()
 init_tables()
 
 
-@app.post('/services/register', response_model=OMService)
-def register_service(
-        om_service: OMServiceCreate, db: Session = Depends(get_db)):
+@app.post('/users/register', response_model=TopioUser)
+def register_user(topio_user: TopioUserCreate, db: Session = Depends(get_db)):
+    topio_user_orm = TopioUserORM(
+        name=topio_user.name, user_namespace=topio_user.user_namespace)
 
-    om_service_orm = OMServiceORM(address=om_service.address)
-    db.add(om_service_orm)
+    db.add(topio_user_orm)
     db.commit()
-    db.refresh(om_service_orm)
+    db.refresh(topio_user_orm)
 
-    return om_service_orm
+    return topio_user_orm
 
 
-@app.get('/services/{service_id}', response_model=OMService)
-def get_service_info(om_service_id: int, db: Session = Depends(get_db)):
-    om_service_orm = \
-        db.query(OMServiceORM).filter(OMServiceORM.id == om_service_id).first()
+@app.get('/users/{topio_user_id}', response_model=TopioUser)
+def get_user_info(topio_user_id: int, db: Session = Depends(get_db)):
+    topio_user_orm = \
+        db.query(TopioUserORM).filter(TopioUserORM.id == topio_user_id).first()
 
-    return om_service_orm
+    return topio_user_orm
 
+
+@app.post('/asset_types/register', response_model=TopioAssetType)
+def register_asset_type(
+        topio_asset_type: TopioAssetType, db: Session = Depends(get_db)):
+
+    topio_asset_type_orm = TopioAssetTypeORM(
+        id=topio_asset_type.id, description=topio_asset_type.description)
+
+    db.add(topio_asset_type_orm)
+    db.commit()
+    db.refresh(topio_asset_type_orm)
+
+    return topio_asset_type_orm
+
+
+@app.get('/asset_types/{topio_asset_type_id}', response_model=TopioAssetType)
+def get_asset_namespace_info(
+        topio_asset_type_id: str, db: Session = Depends(get_db)):
+
+    topio_asset_type_orm = db\
+        .query(TopioAssetTypeORM)\
+        .filter(TopioAssetTypeORM.id == topio_asset_type_id)\
+        .first()
+
+    return topio_asset_type_orm
+
+
+@app.get('/asset_types/', response_model=List[TopioAssetType])
+def get_asset_types(db: Session = Depends(get_db)):
+    return db.query(TopioAssetTypeORM).all()
+
+
+@app.post('/assets/register', response_model=TopioAsset)
+def register_asset(topio_asset: TopioAssetCreate, db: Session = Depends(get_db)):
+    topio_asset_orm = TopioAssetORM(
+        local_id=topio_asset.local_id,
+        owner_id=topio_asset.owner_id,
+        asset_type=topio_asset.asset_type,
+        description=topio_asset.description)
+
+    db.add(topio_asset_orm)
+    db.commit()
+    db.refresh(topio_asset_orm)
+
+    return topio_asset_orm
+
+
+@app.post('/assets/topio_id', response_model=str)
+def get_topio_id(
+        asset_info: TopioAssetCreate,
+        db: Session = Depends(get_db)):
+
+    asset = db\
+        .query(TopioAssetORM)\
+        .filter(TopioAssetORM.owner_id == asset_info.owner_id and
+                TopioAssetORM.asset_type == asset_info.asset_type and
+                TopioAssetORM.local_id == asset_info.local_id)\
+        .first()
+
+    return asset.topio_id
+
+
+@app.post('/assets/', response_model=List[TopioAsset])
+def get_users_assets(user: TopioUserQuery, db: Session = Depends(get_db)):
+    return db\
+        .query(TopioAssetORM)\
+        .filter(TopioAssetORM.owner_id == user.user_id)\
+        .all()
--- ompid/models/__init__.py
+++ ompid/models/__init__.py
@@ -1,27 +1,129 @@
-import pydantic
-from sqlalchemy import Column
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import Integer, String
+import re
+from typing import Optional
 
+import pydantic
+from pydantic import validator
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import select, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import column_property
+from sqlalchemy.sql.sqltypes import Integer, String
 
 Base = declarative_base()
 
 
-class OMServiceORM(Base):
-    __tablename__ = 'op_ext_service'
+class TopioUserORM(Base):
+    """
+    A top.io user is a person or organization that uses the top.io portal.
+    A user is assumed to have a name and a user namespace which will be used to
+    generate asset IDs for a user. Moreover, the user will get a top.io user ID
+    during registration.
+    """
+    __tablename__ = 'topio_user'
 
     id = Column(Integer, primary_key=True, index=True)
-    # TODO: PW: Maybe it makes more sense to store the API description instead
-    address = Column(String)
+    name = Column(String, unique=True)
+    user_namespace = Column(String, unique=True)
 
 
-class OMServiceCreate(pydantic.BaseModel):
-    address: str
+class TopioUserQuery(pydantic.BaseModel):
+    user_id: int
 
 
-class OMService(OMServiceCreate):
+class TopioUserCreate(pydantic.BaseModel):
+    name: str
+    user_namespace: str
+
+    @validator('user_namespace')
+    def _validate_user_namespace(cls, ns):
+        if re.search(r'\s', ns):
+            raise ValueError('user namespace must not contain whitespace')
+        return ns
+
+
+class TopioUser(TopioUserCreate):
     id: int
 
     class Config:
         orm_mode = True
 
+
+class TopioAssetTypeORM(Base):
+    """
+    A top.io asset type is a category or class summarizing a set of similar
+    assets. The main proposed categories/types so far are
+
+      - dataset
+      - service
+
+    In terms of PID creation they are mainly used as second-level namespace
+    identifier used after the top level user namespace, e.g.
+
+      user23.dataset.345
+
+    Assets types are registered globally s.t. they are available to all users.
+    """
+    __tablename__ = 'topio_asset_type'
+
+    id = Column(String, primary_key=True)
+    description = Column(String)
+
+
+class TopioAssetType(pydantic.BaseModel):
+    id: str
+    description: Optional[str] = None
+
+    @validator('id')
+    def _validate_asset_type_id(cls, asset_type_id):
+        if re.search(r'\s', asset_type_id):
+            raise ValueError(
+                'Asset type identifier must not contain whitespace')
+
+        return asset_type_id
+
+    class Config:
+        orm_mode = True
+
+
+class TopioAssetORM(Base):
+    """
+    A top.io asset is any artifact deployed on the top.io platform, be it a
+    dataset, a service etc. A top.io asset has an asset type which is one of the
+    TopioAssetType entries registered via the PID service. Moreover it may have
+    some kind of identifier or name which we treat as a *local* identifier valid
+    in a certain realm, e.g. the primary key in a database table, a file name,
+    or an HDFS URI. What the PID service provides during registration of such an
+    asset is a *global* identifier which is valid everywhere in the top.io
+    platform. Further it provides means to translate between local and global
+    identifiers.
+    """
+    __tablename__ = 'topio_asset'
+
+    _id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    local_id = Column(String)
+    owner_id = Column(Integer, ForeignKey('topio_user.id'))
+    asset_type = Column(String, ForeignKey('topio_asset_type.id'))
+    description = Column(String)
+    user_ns = column_property(
+        select([TopioUserORM.user_namespace])
+        .where(TopioUserORM.id == owner_id).as_scalar())
+
+    @hybrid_property
+    def topio_id(self):
+        return f'{self.user_ns}.{self.asset_type}.{self._id}'
+
+
+class TopioAssetCreate(pydantic.BaseModel):
+    local_id: Optional[str] = None
+    owner_id: int
+    asset_type: str
+    description: Optional[str] = None
+
+
+class TopioAsset(TopioAssetCreate):
+    _id: int
+    topio_id: str
+
+    class Config:
+        orm_mode = True
